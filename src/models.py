e1b0ba3c2aac98e29ef286a1b86755b96ce81ff9
Martín Pravia <marto.pravia@gmail.com>
2024-12-02 19:59:39 +0000

    nuevos cambios (saco mayusculas y paso a plural los tablenames

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "User"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     username = Column(String(120), nullable=False)
     firstname = Column(String(120), nullable=False)
@@ -16,34 +16,34 @@ class User(Base):
     email = Column(String(120), unique=True, nullable=False)
     
 class Comment(Base):
-    __tablename__ = "Comment"
+    __tablename__ = "comments"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     comment_text = Column(String)
-    author_id = Column(Integer, ForeignKey("User.id"))
-    post_id = Column(Integer, ForeignKey("Post.id"))
-    user = relationship("User", backref="comment")
-    post = relationship("Post", backref = "comment")
+    author_id = Column(Integer, ForeignKey("users.id"))
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user = relationship("User", backref="comments")
+    post = relationship("Post", backref = "comments")
 
 class Post(Base):
-    __tablename__ = "Post"
+    __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
-    user = relationship("User", backref="post")
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", backref="posts")
 
 class Media(Base):
-    __tablename__ = "Media"
+    __tablename__ = "medias"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    type = Column(Enum)
-    url = Column(String(150))
-    post_id = Column(Integer, ForeignKey("Post.id"), nullable=False)
-    post = relationship("Post", backref="media")
+    type = Column(Enum, nullable=False)
+    url = Column(String(150), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    post = relationship("Post", backref="medias")
 
 class Follower(Base):
-    __tablename__ = "Follower"
-    user_form_id = Column(Integer, ForeignKey("User.id"), primary_key=True, nullable=False, autoincrement=True)
-    user_to_id = Column(Integer,ForeignKey("User.id"), primary_key= True, nullable=False, autoincrement=True)
-    user_form = relationship("User", backref="follower")
-    user_to = relationship("User", backref="follower")
+    __tablename__ = "followers"
+    user_form_id = Column(Integer, ForeignKey("users.id"), primary_key=True, nullable=False)
+    user_to_id = Column(Integer,ForeignKey("users.id"), primary_key= True, nullable=False)
+    user_form = relationship("User", backref="following")
+    user_to = relationship("User", backref="followers")
     
         
 
